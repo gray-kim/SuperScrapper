873a87229a77c72fe397e45cd958da6ac054eba0
gray-kim <egoist3.work@gmail.com>
2021-03-26 22:24:53 +0900

    pycham

--- main.py
+++ main.py
@@ -6,44 +6,46 @@ app = Flask("SuperScrapper")
 
 db = {}
 
+
 @app.route("/")
 def home():
-  return render_template("potato.html")
+    return render_template("potato.html")
+
 
 @app.route("/report")
 def report():
-  word = request.args.get('word')
-  if word:
-    word = word.lower()
-    existingJobs = db.get(word)
-    if existingJobs:
-      jobs = existingJobs
+    word = request.args.get('word')
+    if word:
+        word = word.lower()
+        existingJobs = db.get(word)
+        if existingJobs:
+            jobs = existingJobs
+        else:
+            jobs = get_jobs(word)
+            db[word] = jobs
     else:
-      jobs = get_jobs(word)
-      db[word] = jobs
-  else:
-    return redirect("/")
-  return render_template(
-    "report.html", 
-    searchingBy=word,
-    resultsNumber=len(jobs),
-    jobs = jobs)
+        return redirect("/")
+    return render_template(
+        "report.html",
+        searchingBy=word,
+        resultsNumber=len(jobs),
+        jobs=jobs)
+
 
 @app.route("/export")
 def export():
-  try:
-    word = request.args.get('word')
-    if not word:
-      raise Exception()
-    word = word.lower()
-    jobs = db.get(word)
-    if not jobs:
-      raise Exception()
-    save_to_file(jobs)
-    return send_file("jobs.csv")
-  except:
-    return redirect("/")
-  
+    try:
+        word = request.args.get('word')
+        if not word:
+            raise Exception()
+        word = word.lower()
+        jobs = db.get(word)
+        if not jobs:
+            raise Exception()
+        save_to_file(jobs)
+        return send_file("jobs.csv")
+    except:
+        return redirect("/")
 
 
 app.run(host="0.0.0.0", port=5000)
